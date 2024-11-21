6da0439414e9025fd3ef4dba47bbeb4caa96fc0a
malak <malakawer886@gmail.com>
2024-11-21 17:18:16 +0300

    ssda

---
+++ lib/core/constans/tasky_images.dart
@@ -0,0 +1,46 @@
+class TaskyImages {
+  //onboard _1
+  static String focaltext = 'assets/images/focal text.png';
+  static String Circle = 'assets/images/Circle.png';
+  static String Circle2 = 'assets/images/Circle2.png';
+  static String man_1 = 'assets/images/man_1.png';
+  static String man_2 = 'assets/images/man_2.png';
+  static String image_box1 = 'assets/images/image box 1.png';
+  static String image_box2 = 'assets/images/Image box 2.png';
+  static String image_box3 = 'assets/images/Image box 3.png';
+  static String image_box4 = 'assets/images/image box 4.png';
+  static String user_profile_1 = 'assets/images/user1.png';
+  static String user_rofile_2 = 'assets/images/user2.png';
+
+  //Home
+  static String user3 = 'assets/images/user3.png';
+  static String user8 = 'assets/images/user8.png';
+  static String user4 = 'assets/images/user4.png';
+
+//project
+  static String project_user4 = 'assets/images/user4.png';
+  static String project_user5 = 'assets/images/user5.png';
+
+  //addtask
+  static String addtask_user4 = 'assets/images/user4.png';
+  static String addtask_user5 = 'assets/images/user5.png';
+  static String addtask_user6 = 'assets/images/user6.png';
+
+  //creat team
+  static String creat_team_user4 = 'assets/images/user4.png';
+  static String creat_team_user5 = 'assets/images/user5.png';
+  static String create_team_user6 = 'assets/images/user6.png';
+  static String create_team_user8 = 'assets/images/user8.png';
+
+  //chat
+
+  static String chat_user4 = 'assets/images/user4.png';
+  static String chat_user5 = 'assets/images/user5.png';
+  static String chat_user6 = 'assets/images/user6.png';
+  static String chat_user8 = 'assets/images/user8.png';
+  static String chat_focal_user = 'assets/images/focal_user.png';
+
+  //search
+  static String search_user5 = 'assets/images/user5.png';
+  static String search_user4 = 'assets/images/user4.png';
+}
