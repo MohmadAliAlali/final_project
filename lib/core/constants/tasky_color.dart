2c06e6bbc51b83d46f1160b75a3fdd3e7d8e8151
Halah Ahmad112 <halahahmad1012000@gmail.com>
2024-11-21 14:59:07 +0300

    halaAhmad

---
+++ lib/core/constants/tasky_color.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class TaskyColor {
+  static const appNameColor = Color(0xFFFF8500);
+  static const welcomePagesTitleColor = Color(0xFF282828);
+
+  static const welcomePagesSubTitleColor = Color(0xFF8D8D8D);
+
+  static const taskyAppButtonTextColor = Color(0xFFFFFFFF);
+
+  static const taskManagementTextColor = Color(0xFFFF8500);
+
+  static const threeOnboardingTextColor = Color(0xFF282828);
+  static const differentOrangeWordsTextColor = Color(0xFFFF8500);
+  static const skipOnboardingTextColor = Color(0xFF282828);
+
+  static const signinSignupTextColor = Color(0xFF002055);
+  static const signinSignupTitleTextColor = Color(0xFF002055);
+  static const signinSignupsubTitleTextColor = Color(0xFF868D95);
+  static const signinSignupInputFieldTextColor = Color(0xFF002055);
+  static const signinSignupHintInputFieldTextColor = Color(0xFF868D95);
+  static const titleHomePageTextTextColor = Color(0xFF002055);
+}
---
+++ lib/core/constants/tasky_text_style.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+
+import 'package:tasky_project/core/constants/tasky_color.dart';
+
+class TaskyTextStyle {
+  static TextStyle nameApp = const TextStyle(
+    fontFamily: 'PollerOne-Regular',
+    fontSize: 46,
+    fontWeight: FontWeight.w400,
+    color: TaskyColor.appNameColor,
+  );
+  static TextStyle welcomePagesTitle = const TextStyle(
+    fontFamily: 'Poppins',
+    fontSize: 37,
+    fontWeight: FontWeight.w400,
+    color: TaskyColor.welcomePagesTitleColor,
+  );
+  static TextStyle welcomePagesSubTitle = const TextStyle(
+    fontFamily: 'Poppins',
+    fontSize: 37,
+    fontWeight: FontWeight.w700,
+    color: TaskyColor.welcomePagesSubTitleColor,
+  );
+  static TextStyle taskyAppButtonTextStyle = const TextStyle(
+    fontFamily: 'Montserrat-Regular',
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+    color: TaskyColor.taskyAppButtonTextColor,
+  );
+  static TextStyle taskManagementTextStyle = const TextStyle(
+    fontFamily: 'Poppins',
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+    color: TaskyColor.taskManagementTextColor,
+  );
+
+  static TextStyle threeOnboardingTextStyle = const TextStyle(
+    fontFamily: 'Poppins-Regular',
+    fontSize: 35,
+    fontWeight: FontWeight.w500,
+    color: TaskyColor.taskManagementTextColor,
+  );
+  static TextStyle differentOrangeWordsTextStyle = const TextStyle(
+    fontFamily: 'Poppins-Bold',
+    fontSize: 35,
+    fontWeight: FontWeight.w600,
+    color: TaskyColor.differentOrangeWordsTextColor,
+  );
+  static TextStyle skipOnboardingTextStyle = const TextStyle(
+    fontFamily: 'Poppins-Regular',
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    color: TaskyColor.skipOnboardingTextColor,
+  );
+
+  static TextStyle signinSignupTextStyle = const TextStyle(
+    fontFamily: 'Poppins-Regular',
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+    color: TaskyColor.signinSignupTextColor,
+  );
+  static TextStyle signinSignupTitleTextStyle = const TextStyle(
+    fontFamily: 'Poppins-Regular',
+    fontSize: 25,
+    fontWeight: FontWeight.w600,
+    color: TaskyColor.signinSignupTitleTextColor,
+  );
+  static TextStyle forgetPasswordTextStyle = const TextStyle(
+    fontFamily: 'Poppins-Regular',
+    fontSize: 25,
+    fontWeight: FontWeight.w600,
+    color: TaskyColor.signinSignupTitleTextColor,
+  );
+  static TextStyle signinSignupInputFieldTextStyle = const TextStyle(
+    fontFamily: 'Poppins-Regular',
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    color: TaskyColor.signinSignupInputFieldTextColor,
+  );
+  static TextStyle signinSignupHintInputFieldTextStyle = const TextStyle(
+    fontFamily: 'Poppins-Regular',
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    color: TaskyColor.signinSignupHintInputFieldTextColor,
+  );
+  static TextStyle signinSignupButtonTextStyle = const TextStyle(
+    fontFamily: 'Poppins-Regular',
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    color: TaskyColor.taskyAppButtonTextColor,
+  );
+  static TextStyle signinSignupWithTextStyle = const TextStyle(
+    fontFamily: 'Poppins-Regular',
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    color: TaskyColor.signinSignupsubTitleTextColor,
+  );
+  static TextStyle notRegistrTextStyle = const TextStyle(
+    fontFamily: 'Poppins-Regular',
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: TaskyColor.signinSignupsubTitleTextColor,
+  );
+  static TextStyle signUpTextStyle = const TextStyle(
+    fontFamily: 'Poppins-Regular',
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: TaskyColor.differentOrangeWordsTextColor,
+  );
+  static TextStyle titleHomePageTextStyle = const TextStyle(
+    fontFamily: 'Poppins-Regular',
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+    color: TaskyColor.titleHomePageTextTextColor,
+  );
+  static TextStyle firstTextHomePageTextStyle = const TextStyle(
+    fontFamily: 'Poppins-Regular',
+    fontSize: 25,
+    fontWeight: FontWeight.w600,
+    color: TaskyColor.titleHomePageTextTextColor,
+  );
+  static TextStyle HomePageTextStyle = const TextStyle(
+    fontFamily: 'Poppins-Regular',
+    fontSize: 25,
+    fontWeight: FontWeight.w600,
+    color: TaskyColor.titleHomePageTextTextColor,
+  );
+}
