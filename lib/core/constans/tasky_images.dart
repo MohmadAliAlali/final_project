c404209de9d8526223c2659f9f92d37cd62ff24e
MohmadAliAlali <mohmad287.ali.alali@gmail.com>
2024-11-21 17:39:17 +0300

    mearge malak branch 2 test

--- lib/core/constans/tasky_images.dart
+++ lib/core/constans/tasky_images.dart
@@ -1,16 +1,16 @@
 class TaskyImages {
   //onboard _1
-  static String focaltext = 'assets/images/focal text.png';
-  static String Circle = 'assets/images/Circle.png';
-  static String Circle2 = 'assets/images/Circle2.png';
+  static String focalText = 'assets/images/focal text.png';
+  static String circle = 'assets/images/Circle.png';
+  static String circle2 = 'assets/images/Circle2.png';
   static String man_1 = 'assets/images/man_1.png';
   static String man_2 = 'assets/images/man_2.png';
-  static String image_box1 = 'assets/images/image box 1.png';
-  static String image_box2 = 'assets/images/Image box 2.png';
-  static String image_box3 = 'assets/images/Image box 3.png';
-  static String image_box4 = 'assets/images/image box 4.png';
-  static String user_profile_1 = 'assets/images/user1.png';
-  static String user_rofile_2 = 'assets/images/user2.png';
+  static String imageBox1 = 'assets/images/image box 1.png';
+  static String imageBox2 = 'assets/images/Image box 2.png';
+  static String imageBox3 = 'assets/images/Image box 3.png';
+  static String imageBox4 = 'assets/images/image box 4.png';
+  static String userProfile_1 = 'assets/images/user1.png';
+  static String userProfile_2 = 'assets/images/user2.png';
 
   //Home
   static String user3 = 'assets/images/user3.png';
@@ -18,29 +18,31 @@ class TaskyImages {
   static String user4 = 'assets/images/user4.png';
 
 //project
-  static String project_user4 = 'assets/images/user4.png';
-  static String project_user5 = 'assets/images/user5.png';
+  static String projectUser4 = 'assets/images/user4.png';
+  static String projectUser5 = 'assets/images/user5.png';
 
-  //addtask
-  static String addtask_user4 = 'assets/images/user4.png';
-  static String addtask_user5 = 'assets/images/user5.png';
-  static String addtask_user6 = 'assets/images/user6.png';
+  //add task
+  static String addTaskUser4 = 'assets/images/user4.png';
+  static String addTaskUser5 = 'assets/images/user5.png';
+  static String addTaskUser6 = 'assets/images/user6.png';
 
-  //creat team
-  static String creat_team_user4 = 'assets/images/user4.png';
-  static String creat_team_user5 = 'assets/images/user5.png';
-  static String create_team_user6 = 'assets/images/user6.png';
-  static String create_team_user8 = 'assets/images/user8.png';
+  //create team
+  static String createTeamUser4 = 'assets/images/user4.png';
+  static String createTeamUser5 = 'assets/images/user5.png';
+  static String createTeamUser6 = 'assets/images/user6.png';
+  static String createTeamUser8 = 'assets/images/user8.png';
 
   //chat
 
-  static String chat_user4 = 'assets/images/user4.png';
-  static String chat_user5 = 'assets/images/user5.png';
-  static String chat_user6 = 'assets/images/user6.png';
-  static String chat_user8 = 'assets/images/user8.png';
-  static String chat_focal_user = 'assets/images/focal_user.png';
+  static String chatUser4 = 'assets/images/user4.png';
+  static String chatUser5 = 'assets/images/user5.png';
+  static String chatUser6 = 'assets/images/user6.png';
+  static String chatUser8 = 'assets/images/user8.png';
+  static String chatFocalUser = 'assets/images/focal_user.png';
 
   //search
-  static String search_user5 = 'assets/images/user5.png';
-  static String search_user4 = 'assets/images/user4.png';
+  static String searchUser5 = 'assets/images/user5.png';
+  static String searchUser4 = 'assets/images/user4.png';
+  //profile
+  static String profileUser4 = 'assets/images/tasky_images.dart';
 }
